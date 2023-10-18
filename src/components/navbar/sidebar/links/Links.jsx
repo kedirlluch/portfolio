8b2184f9636c448cb0fb804bd69c32b503e40fce
Khellum98 <65533924+kedirlluch@users.noreply.github.com>
2023-10-18 16:35:39 +0200

    act

---
+++ src/components/navbar/sidebar/links/Links.jsx
@@ -0,0 +1,16 @@
+
+
+export const Links = () => {
+  const items = [
+    "Homepage",
+    "Services",
+    "Portfolio",
+    "Contact",
+    "About",
+  ]
+  return (
+    <div className="links">{items.map(item=>(
+      <a href={`#${item}`} key={item}>{item}</a>
+    ))}</div>
+  )
+}
